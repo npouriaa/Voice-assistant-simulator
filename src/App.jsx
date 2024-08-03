e5e991a3ef718a9b098ac1e6c985536ccaba6134
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-08-03 17:05:05 +0330

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import "./App.css";
 
 const App = () => {
   const [recording, setRecording] = useState(false);
-  const [language, setLanguage] = useState("en-US");
-  const [answerMode, setAnswerMode] = useState("text");
+  const [responseMode, setResponseMode] = useState("text");
   const [transcription, setTranscription] = useState("");
   const [audioURL, setAudioURL] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
@@ -29,33 +28,40 @@ const App = () => {
       .then((text) => eval(`(${text})`))
       .then((res) => {
         setMessage(res);
-        if (answerMode === "audio") {
+        if (responseMode === "audio") {
           msg.text = res.message;
+          msg.onend = () => {
+            setMessage("");
+          };
           window.speechSynthesis.speak(msg);
         }
       })
-      .catch((error) => {
-        console.error(error);
+      .catch((err) => {
+        console.error(err);
+        setErrorMessage(err.message);
         setLoading(false);
       });
 
-    console.log("first");
     setLoading(false);
   };
 
   recognition.onresult = (event) => {
     const transcript = event.results[0][0].transcript;
     setTranscription(transcript);
-    if (answerMode === "audio") {
+    if (responseMode === "audio") {
       setTimeout(() => {
         audioRef.current
           .play()
           .then(() => {
-            audioRef.current.addEventListener("ended", () => getMessage());
+            audioRef.current.addEventListener("ended", () => {
+              getMessage();
+              setAudioURL("");
+            });
             setMessage("");
           })
-          .catch((error) => {
-            console.error("Audio playback failed:", error);
+          .catch((err) => {
+            console.error("Audio playback failed:", err);
+            setErrorMessage("Audio playback failed:", err.message);
           });
       }, 3000);
     } else {
@@ -78,19 +84,17 @@ const App = () => {
   };
 
   const startRecording = () => {
+    recognition.start();
     setTranscription("");
+    setRecording(true);
     setErrorMessage("");
     setMessage("");
     mediaRecorderRef.current.start();
-    recognition.start();
-    setRecording(true);
   };
 
   const stopRecording = () => {
-    mediaRecorderRef.current.stop();
-    recognition.abort();
-    recognition.stop();
     setRecording(false);
+    mediaRecorderRef.current.stop();
   };
 
   useEffect(() => {
@@ -103,15 +107,16 @@ const App = () => {
       };
 
       mediaRecorderRef.current.onstop = () => {
-        const blob = new Blob(audioChunksRef.current, { type: "audio/wav" });
+        const blob = new Blob(audioChunksRef.current, { type: "audio/mp3" });
         const url = URL.createObjectURL(blob);
         setAudioURL(url);
         audioChunksRef.current = [];
       };
     };
 
-    getUserMedia().catch((error) => {
-      console.error("Error accessing audio:", error);
+    getUserMedia().catch((err) => {
+      console.error("Error accessing audio:", err);
+      setErrorMessage(err.message);
     });
 
     return () => {
@@ -125,106 +130,129 @@ const App = () => {
 
   useEffect(() => {
     setMessage("");
-  }, [answerMode]);
+    setTranscription("");
+    setAudioURL("");
+  }, [responseMode]);
 
   return (
     <div className="app-con">
-      <h1>Voice Assistant Simulator</h1>
-      <div className="content">
-        <div className="response-mode row">
+      <div className="header">
+        <h2>Voice Assistant Simulator</h2>
+        <div className="response-mode">
           <label htmlFor="">Response mode : </label>
           <div className="switch-response-mode-con">
-            text :
-            <input
-              checked={answerMode === "text"}
-              type="radio"
-              name="response"
-              id=""
-              onChange={() => setAnswerMode("text")}
-            />
-            audio :
-            <input
-              type="radio"
-              name="response"
-              id=""
-              onChange={() => setAnswerMode("audio")}
-            />
+            <div className="switch-response-mode">
+              <input
+                type="radio"
+                id="text-mode"
+                name="response-mode"
+                checked={responseMode === "text"}
+                onChange={() => setResponseMode("text")}
+              />
+              <label htmlFor="text-mode">Text</label>
+              <input
+                type="radio"
+                id="audio-mode"
+                name="response-mode"
+                onChange={() => setResponseMode("audio")}
+              />
+              <label htmlFor="audio-mode">Audio</label>
+            </div>
           </div>
         </div>
-        <div className="recorder row">
-          <button
-            onClick={recording ? stopRecording : startRecording}
-            className="recorder-btn"
-          >
-            {recording ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
-                />
-              </svg>
-            )}
-          </button>
-          {recording && <span className="is-recording"></span>}
-        </div>
       </div>
-      <p>Transcription : {transcription}</p>
-      <div className="response">
-        {answerMode === "text" ? (
-          message && (
-            <div className="typewriter">
-              <h1>{message.message}</h1>
-            </div>
-          )
+      <div
+        className={`response ${responseMode === "audio" && "audio-response"}`}
+      >
+        {
+          !audioURL && !message ?
+          <h3 className="assistant-default-text">How can I assist you ?</h3> : ""
+        }
+        {errorMessage ? (
+          <h3 className="error-message">{errorMessage}</h3>
         ) : (
           <>
-            {answerMode === "audio" && (
-              <audio
-                style={{ display: "none" }}
-                ref={audioRef}
-                controls
-                src={audioURL}
-              ></audio>
-            )}
-            {message && (
-              <div id="bars">
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-              </div>
+            {responseMode === "text" ? (
+              transcription && (
+                <>
+                  <p>You : {transcription}</p>
+                  <p>
+                    <span className="assistant-response">Assistant :</span>{" "}
+                    {loading ? <span class="isTyping"></span> : message.message}
+                  </p>
+                </>
+              )
+            ) : (
+              <>
+                {audioURL && (
+                  <audio ref={audioRef} controls src={audioURL}></audio>
+                )}
+                {loading ? (
+                  <div className="isTyping-con">
+                    <span class="isTyping"></span>
+                  </div>
+                ) : (
+                  message && (
+                    <div id="visualizer">
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                      <div className="bar"></div>
+                    </div>
+                  )
+                )}
+              </>
             )}
           </>
         )}
-        {loading && <p>Please wait</p>}
-        {errorMessage && <p>Error : {errorMessage}</p>}
+      </div>
+      <div className={`recorder ${recording && "is-recording"}`}>
+        <button
+          onClick={recording ? stopRecording : startRecording}
+          className="recorder-btn"
+        >
+          {recording ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
+              />
+            </svg>
+          )}
+        </button>
+        {recording && (
+          <>
+            <span className="blink-on-recording"></span>
+            <p>Recording</p>
+          </>
+        )}
       </div>
     </div>
   );
